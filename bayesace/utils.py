0990d5560fa1bd22ddbcf35da998c99918a3d929
Enrique-Val <enrique.valero@hotmail.com>
2024-03-04 17:25:53 +0100

    Training bug solved

--- bayesace/utils.py
+++ bayesace/utils.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
+import pybnesian as pb
 import warnings
 import math
+import multiprocessing as mp
+import openml as oml
+from sklearn.preprocessing import StandardScaler
 
 
 def identity(x):
@@ -114,3 +118,75 @@ def path_likelihood_length(path: pd.DataFrame, bayesian_network, penalty=1):
     medium_points = ((path + path.shift()) / 2).drop(0).reset_index()
     likelihood_path = (-log_likelihood(medium_points, bayesian_network)) ** penalty * separation
     return np.sum(likelihood_path)
+
+
+def get_naive_structure(data: pd.DataFrame, type):
+    naive = type(data.columns)
+    for i in [i for i in data.columns if i != "class"]:
+        naive.add_arc("class", i)
+    return naive
+
+
+def copy_structure(bn: pb.BayesianNetwork):
+    copy = type(bn)(bn.nodes())
+    for i in bn.arcs():
+        copy.add_arc(i[0], i[1])
+    return copy
+
+
+def check_copy(bn):
+    return bn.fitted()
+
+
+def hill_climbing(data: pd.DataFrame, bn_type: str, score=None, seed=0):
+    bn = None
+    if bn_type == "CLG":
+        if score is None:
+            score = "bic"
+        bn = pb.hc(data, start=get_naive_structure(data, pb.CLGNetwork), operators=["arcs"], score=score,
+                   seed=seed)
+        bn = copy_structure(bn)
+    elif bn_type == "SP":
+        if score is None:
+            score = "validated-lik"
+        bn = pb.hc(data, start=get_naive_structure(data, pb.SemiparametricBN), operators=["arcs", "node_type"],
+                   score=score,
+                   seed=seed)
+        bn = copy_structure(bn)
+    elif bn_type == "Gaussian":
+        if score is None:
+            score = "bic"
+        bn = pb.hc(data, start=get_naive_structure(data, pb.GaussianNetwork), operators=["arcs"], score=score,
+                   seed=seed)
+        bn = copy_structure(bn)
+    else:
+        raise PybnesianParallelizationError(
+            "Only valid types are CLG, SP and Gaussian. For more customization use the hc method of pybnesian")
+    bn.fit(data)
+    pool = mp.Pool(1)
+    res = pool.starmap(check_copy, [(bn,)])
+    pool.close()
+    if not res[0]:
+        raise PybnesianParallelizationError(
+            "As of version 0.4.3, PyBnesian Bayesian networks have internal and stochastic problems with the method "
+            "\"copy()\"."
+            "As such, the network is not parallelized correctly and experiments cannot be launched.")
+    return bn
+
+
+def get_and_process_data(dataset_id: int):
+    # Load the dataset
+    data = oml.datasets.get_dataset(dataset_id, download_data=True, download_qualities=False,
+                                    download_features_meta_data=False).get_data()[0]
+
+    # Shuffle the dataset
+    data = data.sample(frac=1, random_state=0)
+
+    # Transform the class into a categorical variable
+    data["class"] = data[data.columns[-1]].astype('string').astype('category')
+    data = data.drop(data.columns[-2], axis=1)
+
+    # Scale the rest of the dataset
+    feature_columns = [i for i in data.columns if i != "class"]
+    data[feature_columns] = StandardScaler().fit_transform(data[feature_columns].values)
+    return data
--- experiment1.py
+++ experiment1.py
@@ -2,6 +2,7 @@ import random
 import csv
 import os
 import sys
+
 sys.path.append(os.getcwd())
 import argparse
 
@@ -22,22 +23,10 @@ import multiprocessing as mp
 
 import time
 
-
-def get_naive_structure(df: pd.DataFrame, type):
-    naive = SemiparametricBN(df.columns)
-    for i in [i for i in df.columns if i != "class"]:
-        naive.add_arc("class", i)
-    return naive
-
-
-def check_copy(bn):
-    return bn.fitted()
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Arguments")
     parser.add_argument("--dataset_id", nargs='?', default=44091, type=int)
-    parser.add_argument("--network", nargs='?', default="G", type=str)
+    parser.add_argument("--network", nargs='?', default="CLG", type=str)
     args = parser.parse_args()
 
     dataset_id = args.dataset_id
@@ -46,67 +35,23 @@ if __name__ == "__main__":
     random.seed(0)
 
     # Load the dataset
-    df = oml.datasets.get_dataset(dataset_id, download_data=True, download_qualities=False,
-                                  download_features_meta_data=False).get_data()[0]
-
-    # Shuffle the dataset
-    df = df.sample(frac=1, random_state=0)
-
-    # Transform the class into a categorical variable
-    df["class"] = df[df.columns[-1]].astype('string').astype('category')
-    df = df.drop(df.columns[-2], axis=1)
-
-    # Scale the rest of the dataset
-    feature_columns = [i for i in df.columns if i != "class"]
-    df[feature_columns] = StandardScaler().fit_transform(df[feature_columns].values)
+    df = get_and_process_data(dataset_id)
 
     # Split the dataset into train and test. Test only contains the 5 counterfactuals to be evaluated
     n_counterfactuals = 5
     df_train = df.head(len(df.index) - n_counterfactuals)
     df_test = df.tail(n_counterfactuals)
 
-    network = None
-    # Train a conditional linear Gaussian network
-    if network_type == "G":
-        clg_network = hc(df_train, start=get_naive_structure(df_train, CLGNetwork), operators=["arcs"], score="bic",
-                         seed=0)
-        clg_network.fit(df_train)
-        # Because of a Pybnesian bug, the copy method does not work properly at times. We have to shut down the experiment in that case
-        pool = mp.Pool(1)
-        res = pool.starmap(check_copy, [(clg_network,)])
-        pool.close()
-        if not res[0]:
-            raise PybnesianParallelizationError(
-                "As of version 0.4.3, PyBnesian Bayesian networks have internal and stochastic problems with the method "
-                "\"copy()\"."
-                "As such, the CLG network is not parallelized correctly and experiments cannot be launched.")
-        network = clg_network
-
-    if network_type == "S":
-        # Train a semiparametric network
-        spb_network = hc(df_train, start=get_naive_structure(df_train, SemiparametricBN),
-                         operators=["node_type", "arcs"],
-                         score="validated-lik", seed=0)
-        spb_network.fit(df_train)
-        # Because of a Pybnesian bug, the copy method does not work properly at times. We have to shut down the experiment in that case
-        pool = mp.Pool(1)
-        res = pool.starmap(check_copy, [(spb_network,)])
-        pool.close()
-        if not res[0]:
-            raise PybnesianParallelizationError(
-                "As of version 0.4.3, PyBnesian Bayesian networks have internal and stochastic problems with the method "
-                "\"copy()\"."
-                "As such, the SPB network is not parallelized correctly and experiments cannot be launched.")
-        network = spb_network
+    network = hill_climbing(data=df_train, bn_type=network_type)
 
     np.random.seed(0)
     # Algorithm parameters (relatively high restriction on accuracy and likelihood)
     likelihood_threshold = 0.2 ** (len(df_train.columns) - 1)
     accuracy_threshold = 0.01
     n_vertices = [0, 1, 2, 3, 4, 5]
-    penalties = [1,2,3,4,5,7.5,10]#,15]
+    penalties = [1, 5, 10, 15, 20]
     chunks = 10
-    
+
     np.seterr(divide='ignore')
     for penalty in penalties:
         # Result storage
@@ -144,7 +89,8 @@ if __name__ == "__main__":
         evaluations_mean = evaluations_mat.mean(axis=0)
         evaluations_std = evaluations_mat.std(axis=0)
 
-        with open('./results/exp_1/data'+str(dataset_id)+'_net'+network_type+'_penalty'+str(penalty)+'.csv', 'w') as f:
+        with open('./results/exp_1/data' + str(dataset_id) + '_net' + network_type + '_penalty' + str(penalty) + '.csv',
+                  'w') as f:
             w = csv.writer(f)
             w.writerow(distances_mean)
             w.writerow(evaluations_mean)
