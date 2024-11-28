4a100682ec039463739a3c767e42dfddfd471b93
Enrique Valero-Leal <enrique.valero@hotmail.com>
2024-11-28 21:54:08 +0100

    Applied experiment reformated

--- experiments/experiment_applied.py
+++ experiments/experiment_applied.py
@@ -41,8 +41,9 @@ if __name__ == "__main__":
         q1 = data["class"].quantile(0.33)
         q2 = data["class"].quantile(0.66)
         data["class"] = data["class"].apply(lambda x: "Safe" if x <= q1 else "Regular" if x <= q2 else "Unsafe").astype('category')
+        # Drop rows labelled as "Regular"
+        #data = data[data["class"] != "Regular"]
         data = data[["population","householdsize","pctUrban","medFamInc","PctPopUnderPov","PctLess9thGrade","PctUnemployed","PersPerFam","PctNotSpeakEnglWell","PctPersDenseHous","RentMedian","MedRentPctHousInc","NumInShelters","NumStreet","PopDens","class"]]
-        # data = data[["racepctblack","racePctAsian","class"]]
     # Drop nan rows
     data = data.dropna()
 
@@ -50,7 +51,11 @@ if __name__ == "__main__":
     scaler = StandardScaler()
     data[data.columns[:-1]] = scaler.fit_transform(data[data.columns[:-1]])
 
-    data_train, data_test = data.iloc[:int(0.8*len(data))], data.iloc[int(0.8*len(data)):].reset_index(drop=True)
+    xl = data.drop(columns=['class']).min().values - 0.01
+    xu = data.drop(columns=['class']).max().values + 0.01
+    sampling_range = (xl, xu)
+
+    data_train, data_test = data.iloc[:int(0.8*len(data))], data.iloc[int(0.8*len(data)):]
     print(data_test)
     bn = hill_climbing(data_train, seed=0, bn_type="CLG")
     print(bn.cpd("class"))
@@ -64,45 +69,40 @@ if __name__ == "__main__":
     brier_i = brier_score(data_test["class"].values, predictions)
     print(brier_i)
 
-    # Look for a counterfactual
-    og_instance = data_test.loc[[0]]
-    print(og_instance)
+    data_set_unsafe = data_test[data_test["class"] == "Unsafe"]
+    target_label = "Safe"
 
-    xl = data.drop(columns=['class']).min().values-0.01
-    xu = data.drop(columns=['class']).max().values+0.01
-    sampling_range = (xl, xu)
-    algorithm = BayesACE(bn, data_train.columns[:-1], chunks=10, log_likelihood_threshold=-10, n_vertex=0, accuracy_threshold=0.95, penalty=1, seed=0, verbose=True, parallelize=False,
-                         opt_algorithm=NSGA2, opt_algorithm_params={"pop_size": 100}, sampling_range=sampling_range, generations=100, multi_objective=False)
-    '''algorithm = FACE(density_estimator=bn, features=data_train.columns[:-1], chunks=10,
-               dataset=data_train.drop("class", axis=1),
-               distance_threshold=np.inf, graph_type="integral", f_tilde=None, seed=0,
-               log_likelihood_threshold=-14, accuracy_threshold=0.95, penalty=1, parallelize=False)'''
-    target_label = "Safe"#get_other_class(og_instance["class"].cat.categories, og_instance["class"].values[0])
-    result = algorithm.run(og_instance, target_label)
-    og_instance_transformed = pd.DataFrame(scaler.inverse_transform(og_instance.drop(columns="class")), columns=og_instance.columns[:-1])
-    print(data_og[["state","county","community","communityname"]].loc[[0]])
-    og_instance_transformed["class"] = og_instance["class"]
-    print(og_instance_transformed.iloc[0])
-    print()
-    result_transformed = scaler.inverse_transform(pd.DataFrame(result.counterfactual.values).T)
-    result_transformed = pd.Series(result_transformed[0], index=og_instance.columns[:-1])
-    print("Difference with BayesACE:")
-    print(-og_instance_transformed.iloc[0].drop("class")+result_transformed)
+    chunks = 20
+    logl_thresh = -10
+    pp_thresh = 0.95
 
-    alg_wachter = WachterCounterfactual(density_estimator=bn, features=data_train.columns[:-1],
-               log_likelihood_threshold=-10, accuracy_threshold=0.95, dataset=data_train)
-    result_wachter = alg_wachter.run(og_instance, target_label)
-    result_wachter_transformed = scaler.inverse_transform(pd.DataFrame(result_wachter.counterfactual.values).T)
-    result_wachter_transformed = pd.Series(result_wachter_transformed[0], index=og_instance.columns[:-1])
-    print("Difference with Wachter:")
-    print(-og_instance_transformed.iloc[0].drop("class")+result_wachter_transformed)
+    algorithms = []
+    alg = BayesACE(bn, data_train.columns[:-1], chunks=chunks, log_likelihood_threshold=logl_thresh, n_vertex=0, accuracy_threshold=pp_thresh, penalty=1, seed=0, verbose=True, parallelize=False,
+                             opt_algorithm=NSGA2, opt_algorithm_params={"pop_size": 100}, sampling_range=sampling_range, generations=100, multi_objective=False)
+    algorithms.append(alg)
 
-    algorithm = FACE(density_estimator=bn, features=data_train.columns[:-1], chunks=10,
-                   dataset=data_train.drop("class", axis=1),
-                   distance_threshold=np.inf, graph_type="integral", f_tilde=None, seed=0,
-                   log_likelihood_threshold=-10, accuracy_threshold=0.95, penalty=1, parallelize=False)
-    result = algorithm.run(og_instance, target_label)
-    result_transformed = scaler.inverse_transform(pd.DataFrame(result.counterfactual.values).T)
-    result_transformed = pd.Series(result_transformed[0], index=og_instance.columns[:-1])
-    print("Difference with FACE:")
-    print(-og_instance_transformed.iloc[0].drop("class") + result_transformed)
+    alg = WachterCounterfactual(density_estimator=bn, features=data_train.columns[:-1],
+                   log_likelihood_threshold=logl_thresh, accuracy_threshold=pp_thresh, dataset=data_train)
+    algorithms.append(alg)
+
+    '''alg = FACE(density_estimator=bn, features=data_train.columns[:-1], chunks=10,
+                       dataset=data_train.drop("class", axis=1),
+                       distance_threshold=np.inf, graph_type="kde", f_tilde=None, seed=0,
+                       log_likelihood_threshold=logl_thresh, accuracy_threshold=pp_thresh, penalty=1, parallelize=False)
+    algorithms_str = ["BayesACE", "Wachter", "FACE"]'''
+    algorithms_str = ["BayesACE", "Wachter"]
+    for i in data_set_unsafe.index:
+        # Look for a counterfactual
+        og_instance = data_set_unsafe.loc[[i]]
+        print(data_og[["state", "county", "community", "communityname"]].loc[[i]])
+        og_instance_transformed = pd.DataFrame(scaler.inverse_transform(og_instance.drop(columns="class")),
+                                               columns=og_instance.columns[:-1])
+        og_instance_transformed["class"] = og_instance["class"]
+        print(og_instance_transformed.iloc[0])
+        for algorithm,algorithm_str in zip(algorithms,algorithms_str):
+            result = algorithm.run(og_instance, target_label)
+            result_transformed = scaler.inverse_transform(pd.DataFrame(result.counterfactual.values).T)
+            result_transformed = pd.Series(result_transformed[0], index=og_instance.columns[:-1])
+            print("Difference with", algorithm_str)
+            print(-og_instance_transformed.iloc[0].drop("class")+result_transformed)
+        print()
