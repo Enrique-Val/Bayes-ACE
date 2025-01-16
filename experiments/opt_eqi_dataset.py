9ed77fb8ef39d9632353f05d7a0ac57f8dc5bcc8
Enrique Valero-Leal <enrique.valero@hotmail.com>
2025-01-16 19:12:13 +0100

    NSGA2 optimization for EQI

--- experiments/opt_eqi_dataset.py
+++ experiments/opt_eqi_dataset.py
@@ -23,15 +23,14 @@ from experiments.utils import setup_experiment, get_constraints, check_enough_in
 
 
 # Worker function for parallelization
-def worker(instance: pd.DataFrame, density_estimator_path: str, gt_estimator_path: str, penalty: int,
+def worker(instance: pd.DataFrame, density_estimator_path: str, penalty: int,
            n_vertices: int, ace_params: dict):
     torch.set_num_threads(1)
     density_estimator = pickle.load(open(density_estimator_path, 'rb'))
-    gt_estimator = pickle.load(open(gt_estimator_path, 'rb'))
-    return get_counterfactuals(instance, density_estimator, gt_estimator, penalty, n_vertices, ace_params)
+    return get_counterfactuals(instance, density_estimator, penalty, n_vertices, ace_params)
 
 
-def get_counterfactuals(instance: pd.DataFrame, density_estimator: ConditionalDE, gt_estimator: ConditionalDE,
+def get_counterfactuals(instance: pd.DataFrame, density_estimator: ConditionalDE,
                         penalty: int,
                         n_vertices: int, ace_params: dict):
     distances = np.zeros(n_vertices)
@@ -46,67 +45,62 @@ def get_counterfactuals(instance: pd.DataFrame, density_estimator: ConditionalDE
                        generations=1000, parallelize=False)
         result = alg.run(instance, target_label=target_label)
         tf = time.time() - t0
-        if result.counterfactual is None:
-            distances[n_vertex] = np.nan
-            times[n_vertex] = tf
-        else:
-            path_to_compute = path(result.path.values, chunks=chunks)
-            pll = path_likelihood_length(
-                pd.DataFrame(path_to_compute, columns=instance.columns[:-1]),
-                density_estimator=gt_estimator, penalty=penalty)
-            if pll == np.inf:
-                warnings.warn("Path length over ground truth is infinite for instance " + str(instance.index[0]) + ", "
-                              + str(n_vertex) + " vertices, penalty of " + str(penalty) + "and estimator " + str(
-                    type(density_estimator)))
-            distances[n_vertex] = pll
-            times[n_vertex] = tf
+        distances[n_vertex] = result.distance
+        times[n_vertex] = tf
     return distances, times
 
 
 if __name__ == "__main__":
     # ALGORITHM PARAMETERS The likelihood parameter is relative. I.e. the likelihood threshold will be the mean logl
     # for that class plus "likelihood_threshold_sigma" sigmas of the logl std
-    likelihood_threshold_sigma = -0.5
-    post_prob_threshold_sigma = -0.5
+    likelihood_threshold_sigma = 0.0
+    post_prob_threshold_sigma = 0.0
     n_vertices = 1
-    penalties = [1]
+    penalties = [1,5]
     # Number of points for approximating integrals:
     chunks = 20
     # Number of counterfactuals
     n_counterfactuals = 20
 
     parser = argparse.ArgumentParser(description="Arguments")
-    parser.add_argument("--dataset_id", nargs='?', default=-1, type=int)
     parser.add_argument('--parallelize', action=argparse.BooleanOptionalAction)
     parser.add_argument('--dir_name', nargs='?', default="./results/exp_cv_eqi/", type=str)
+    parser.add_argument('--model', nargs='?', default="all", choices=["all","bn_restricted",
+                                                                      "bn_restricted_lim_arcs","bn_unrestricted"])
     args = parser.parse_args()
 
     model_str: str = args.model
-    dataset_id = args.dataset_id
     parallelize = args.parallelize
+
+    results_dir = args.dir_name + "data_processed/"
+    model_dir = args.dir_name + "models/"
 
     random.seed(0)
 
-    results_cv_dir = args.cv_dir + str(dataset_id) + '/'
-    results_dir = args.results_dir
+    # Load some train data and the different estimators to fine tune a genetic algorithm
+    df_train = pd.read_csv(os.path.join(results_dir, "data_train.csv"), index_col=0)
+    models: [str, ConditionalDE] = {}
+    models_path: [str, ConditionalDE] = {}
+    for model in os.listdir(model_dir):
+        if ".pkl" in model :
+            model_path = os.path.join(model_dir, model)
+            with open(model_path, "rb") as f:
+                models[model[:-4]] = pickle.load(f)
+                models_path[model[:-4]] = model_path
 
-    df_train, df_counterfactuals, gt_estimator, gt_estimator_path, clg_network, clg_network_path, normalizing_flow, nf_path = setup_experiment(
-        results_cv_dir, dataset_id, n_counterfactuals)
-    sampling_range, mu_gt, std_gt, mae_gt, std_mae_gt = get_constraints(df_train, df_counterfactuals, gt_estimator)
+    df_counterfactuals = df_train[int(df_train["class"]) < 5].head(n_counterfactuals)
+
+    sampling_range, mu_gt, std_gt, mae_gt, std_mae_gt = get_constraints(df_train, df_counterfactuals, models["nf"])
     log_likelihood_threshold = mu_gt + likelihood_threshold_sigma * std_gt
     post_prob_threshold = min(mae_gt + post_prob_threshold_sigma * std_mae_gt, 0.99)
     # Check if there are instances with this threshold in the training set
-    check_enough_instances(df_train, gt_estimator, log_likelihood_threshold, post_prob_threshold)
+    for model_name, model in models :
+        check_enough_instances(df_train, model, log_likelihood_threshold, post_prob_threshold)
 
-    if model_str == 'nf':
-        density_estimator_path = nf_path
-        density_estimator = normalizing_flow
-    elif model_str == 'clg':
-        density_estimator_path = clg_network_path
-        density_estimator = clg_network
-    elif model_str == 'gt':
-        density_estimator_path = gt_estimator_path
-        density_estimator = gt_estimator
+    # Reduce the models dict in specified in args
+    if args.model != "all" :
+        models = {args.model : models[args.model]}
+        models_str = {args.model: models_path[args.model]}
 
     param_grid = {
         'eta_crossover': [10, 15, 20],  # Example range for crossover eta
@@ -116,45 +110,49 @@ if __name__ == "__main__":
     }
     param_combinations = ParameterGrid(param_grid)
 
-    results_df = pd.DataFrame(columns=[str(params) for params in param_combinations],
-                              index=range(n_counterfactuals * len(penalties) * n_vertices))
+    for model_str in models.keys():
+        model_path = models_path[model_str]
+        model = models[model_str]
 
-    for params in param_combinations:
-        print("Running with parameters: " + str(params) + " in model " + model_str)
-        # Create dictionary of ace parameters
-        ace_params = {"posterior_probability_threshold": post_prob_threshold,
-                      "log_likelihood_threshold": log_likelihood_threshold, "chunks": chunks,
-                      "sampling_range": sampling_range, "opt_algorithm_params": {
-                "pop_size": 100,
-                "crossover": SBX(eta=params["eta_crossover"]),
-                "mutation": PM(eta=params["eta_mutation"]),
-                "selection": TournamentSelection(func_comp=binary_tournament) if params["selection_type"] == "tourn" else RandomSelection()}}
-        distances = []
-        for penalty in penalties:
-            print("Running with parameters: " + str(params) + "      Penalty " + str(penalty))
-            # Result storage
-            distances_mat = np.zeros((n_counterfactuals, n_vertices))
-            if parallelize:
-                pool = mp.Pool(min(mp.cpu_count() - 1, n_counterfactuals))
-                results = pool.starmap(worker,
-                                       [(df_counterfactuals.iloc[[i]], gt_estimator_path, gt_estimator_path,
-                                         penalty, n_vertices, ace_params) for i in
-                                        range(n_counterfactuals)])
-                pool.close()
-                pool.join()
+        results_df = pd.DataFrame(columns=[str(params) for params in param_combinations],
+                                  index=range(n_counterfactuals * len(penalties) * n_vertices))
 
-                for i in range(n_counterfactuals):
-                    distances_mat[i], _ = results[i]
-            else:
-                for i in range(n_counterfactuals):
-                    instance = df_counterfactuals.iloc[[i]]
-                    distances_mat[i], _ = get_counterfactuals(instance, density_estimator, gt_estimator,
-                                                              penalty, n_vertices, ace_params)
-            distances_pen = distances_mat.flatten()
-            distances.append(distances_pen)
-        distances = np.concatenate(distances)
-        results_df[str(params)] = distances
+        for params in param_combinations:
+            print("Running with parameters: " + str(params) + " in model " + model_str)
+            # Create dictionary of ace parameters
+            ace_params = {"posterior_probability_threshold": post_prob_threshold,
+                          "log_likelihood_threshold": log_likelihood_threshold, "chunks": chunks,
+                          "sampling_range": sampling_range, "opt_algorithm_params": {
+                    "pop_size": 100,
+                    "crossover": SBX(eta=params["eta_crossover"]),
+                    "mutation": PM(eta=params["eta_mutation"]),
+                    "selection": TournamentSelection(func_comp=binary_tournament) if params["selection_type"] == "tourn" else RandomSelection()}}
+            distances = []
+            for penalty in penalties:
+                print("Running with parameters: " + str(params) + "      Penalty " + str(penalty))
+                # Result storage
+                distances_mat = np.zeros((n_counterfactuals, n_vertices))
+                if parallelize:
+                    pool = mp.Pool(min(mp.cpu_count() - 1, n_counterfactuals))
+                    results = pool.starmap(worker,
+                                           [(df_counterfactuals.iloc[[i]], model_path,
+                                             penalty, n_vertices, ace_params) for i in
+                                            range(n_counterfactuals)])
+                    pool.close()
+                    pool.join()
 
-    if not os.path.exists(results_dir):
-        os.makedirs(results_dir)
-    results_df.to_csv(os.path.join(results_dir, 'results_data' + str(dataset_id) + '_' + model_str + '.csv'))
+                    for i in range(n_counterfactuals):
+                        distances_mat[i], _ = results[i]
+                else:
+                    for i in range(n_counterfactuals):
+                        instance = df_counterfactuals.iloc[[i]]
+                        distances_mat[i], _ = get_counterfactuals(instance, model,
+                                                                  penalty, n_vertices, ace_params)
+                distances_pen = distances_mat.flatten()
+                distances.append(distances_pen)
+            distances = np.concatenate(distances)
+            results_df[str(params)] = distances
+
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+        results_df.to_csv(os.path.join(results_dir, 'results_dataEQI_' + model_str + '.csv'))
